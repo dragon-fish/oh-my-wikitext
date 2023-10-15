1f24dcde89f2aad148ffbaeafc816bdb30f7f938
Dragon-Fish <dragon-fish@qq.com>
2023-10-16 05:09:28 +0800

    chore: type gymnastics

--- functions/api.php/[[path]].ts
+++ functions/api.php/[[path]].ts
@@ -1,11 +1,20 @@
-export function onRequest(ctx: { request: Request }): Promise<Response> {
-  const { request } = ctx
+import { EventContext } from '@cloudflare/workers-types'
+
+interface Env {
+  MW_API_ENDPOINT: string
+  MW_ARTICLE_PATH: string
+}
+
+export const onRequest = async (
+  ctx: EventContext<Env, any, any>
+): Promise<Response> => {
+  const request = ctx.request
 
   const url = new URL(request.url)
   url.host = 'zh.moegirl.org.cn'
   url.protocol = 'https:'
   url.port = ''
 
-  const proxy = new Request(url, request)
+  const proxy = new Request(url, request as any)
   return fetch(proxy)
 }
